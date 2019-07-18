689309187a0a518b718a8ccd743c9a445525772c
Christiana Kobiah <christianakobea@gmail.com>
2019-07-18 11:34:08 +0100

    i get it

--- Internet_of_things/myapp.py
+++ Internet_of_things/myapp.py
@@ -1,7 +1,9 @@
 
-from flask import Flask, render_template
+from flask import *
  
 app = Flask(__name__)
+
+items = []
 ''' 
 @app.route('/greetings')
 def index():
@@ -11,10 +13,17 @@ def index():
 def where_i_am():
     return 'ghana'
 '''
-
 @app.route('/')
 def tee():
-    return render_template('index.html')
+    return render_template('form.html', items=items)
+
+@app.route('/add_todo')
+def add_todo():
+    item = request.args.get("item")
+    print(item)
+    items.append(item)
+    return redirect("http://localhost:5000/", code = 302)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
